78f534d0f89cd97458db5873e952bd05a882536f
Alanav29 <josephalannavarro@gmail.com>
2023-06-15 03:44:00 -0600

    Inicia proyecto con navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Index from "./routes/Index";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./styles/appStyle.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Router>
+        <Index />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Movie Time
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
